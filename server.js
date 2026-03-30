36164f950a3d98da5b0d2749cb8bc8f5d83b7f70
couturierbaptiste50-a11y <couturier.baptiste50@gmail.com>
2026-03-30 12:47:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const cors = require('cors');
 const fetch = require('node-fetch');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.options('*', cors());
 app.use(express.json());
 
 // ═══════════════════════════════════════════
